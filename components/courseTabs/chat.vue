abb0add6535e277bc6120b4e849aaa6d4fcbca7b
OoI3enZaoO <Teerapath-Ben@hotmail.com>
2017-09-05 10:31:16 +0700

    add socket.io for chat tabs

--- components/courseTabs/chat.vue
+++ components/courseTabs/chat.vue
@@ -6,7 +6,7 @@
       <v-card>
         <v-card-text>
           <v-list two-line>
-            <template v-for="a in 5">
+            <!-- <template v-for="a in 5">
                 <v-list-tile avatar >
                   <v-list-tile-avatar>
                     <img src="http://s3.amazonaws.com/s3.timetoast.com/public/uploads/photos/2065220/billgates.png?1473908657" alt="avatar">
@@ -16,12 +16,24 @@
                       <v-list-tile-sub-title>มันดีมากๆเลยครับ</v-list-tile-sub-title>
                   </v-list-tile-content>
                 </v-list-tile>
+            </template> -->
+
+            <template v-for="data in message">
+                <v-list-tile avatar >
+                  <v-list-tile-avatar>
+                    <img :src="data.userdata.img" alt="avatar">
+                  </v-list-tile-avatar>
+                  <v-list-tile-content>
+                      <v-list-tile-title>{{data.userdata.name}} &nbsp;&nbsp; <span class="grey--text">10 นาทีที่แล้ว</span> </v-list-tile-title>
+                      <v-list-tile-sub-title>{{data.message}}</v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
             </template>
+
 
           </v-list>
         </v-card-text>
-        <v-text-field solo label="พิมช้อความ">
-        </v-text-field>
+        <v-text-field solo label="พิมช้อความ"  @keyup.enter="sendMessage($event.target.value)"></v-text-field>
       </v-card>
     </v-container>
     <br><br><br>
@@ -29,6 +41,36 @@
 </template>
 <script>
 export default {
+  beforeMount () {
+    this.$socket.emit('subscribe', this.roomId)
+  },
+  mounted () {
+    this.$options.sockets.conversation_private = (data) => {
+      console.log(JSON.stringify(data))
+      this.message.push(data)
+    }
+  },
+  data () {
+    return {
+      roomId: 1212335,
+      userData: {
+        name: 'ben',
+        img: 'https://scontent.fbkk1-3.fna.fbcdn.net/v/t1.0-9/18670848_1440946712632376_9108286887308110690_n.jpg?oh=aed0ea5bba94084d3114d146c6c7a907&oe=5A267C89'
+      },
+      message: []
+    }
+  },
+  methods: {
+    sendMessage (message) {
+      console.log('message: ' + message)
+      const data = {
+        room: this.roomId,
+        message: message,
+        userdata: this.userData
+      }
+      this.$socket.emit('private_message', data)
+    }
+  }
 
 }
 </script>
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import VueSocketio from 'vue-socket.io'
 if (process.browser) {
   const VueQuillEditor = require('vue-quill-editor/ssr')
   Vue.use(VueQuillEditor)
+  Vue.use(VueSocketio, 'http://localhost:1337/')
 }
 Vue.use(Vuetify)
